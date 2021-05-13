6f14087015beb73f7d1d6b42a09dea5768ddfbab
Andrewk311 <Andrewking0619@gmail.com>
2021-05-13 01:59:00 -0400

    Added popup window to add rules, as well as future buttons

--- script.js
+++ script.js
@@ -15,6 +15,34 @@ var mistakes = 0;
 var timeleft = 10;
 var downloadTimer;
 
+//How to play button 
+
+// Get the popup
+var popup = document.getElementById("myPopup");
+
+// Get the button that opens the popup
+var rules = document.getElementById("Rules");
+
+// Get the <span> element that closes the popup
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the popup
+rules.onclick = function() {
+  popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function() {
+  popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+}
+
 
 function startGame(){
     downloadTimer = setInterval(timer,1000);
